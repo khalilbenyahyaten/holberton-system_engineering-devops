b5de47a68ab4ea40c939d43bbb33401b942321f7
khalilbenyahyaten <2300@holbertonschool.com>
2021-04-12 16:21:22 +0000

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file using pp
 file {
     '/tmp/holberton':
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
