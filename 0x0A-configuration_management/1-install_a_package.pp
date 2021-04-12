ea0470e5543db3e4c50ee543e6a2c7663c818ad5
khalilbenyahyaten <2300@holbertonschool.com>
2021-04-12 16:32:58 +0000

    install puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # install puppet-lint
-package {
-    'puppet-lint':
-    ensure      => '2.1.1',
-    provider    => 'gem'   
+package { 'puppet-lint':
+    ensure  => '2.1.1',
+    provider=> 'gem'   
 }
